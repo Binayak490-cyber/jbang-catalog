49dbe7294a11c9f03b93fc21e78c736d80ae9ae0
Binayak490-cyber <binayak490@gmail.com>
2026-08-30 01:53:03 +0530

    debezium/dbz#2241 Auto-detect local Maven repo; initialize ~/.dbz/config.yaml on first run
    Signed-off-by: Binayak490-cyber <binayak490@gmail.com>

--- debezium-jbang/debezium-jbang-core/src/main/java/io/debezium/jbang/core/commands/build/DistributionResolver.java
+++ debezium-jbang/debezium-jbang-core/src/main/java/io/debezium/jbang/core/commands/build/DistributionResolver.java
@@ -92,9 +92,9 @@ public class DistributionResolver {
         command.add(profiles);
         command.add("-DskipTests");
         command.add("--no-transfer-progress");
-        String localRepo = Configuration.load().getMavenLocalRepo();
-        if (localRepo != null && !localRepo.isBlank()) {
-            command.add("-Dmaven.repo.local=" + localRepo);
+        Path defaultLocalRepo = Path.of(System.getProperty("user.home"), ".m2", "repository");
+        if (Files.isDirectory(defaultLocalRepo)) {
+            command.add("-Dmaven.repo.local=" + defaultLocalRepo.toAbsolutePath());
         }
 
         ProcessBuilder pb = new ProcessBuilder(command);
